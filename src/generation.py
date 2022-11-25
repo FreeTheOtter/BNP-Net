2db8c3fe9b7fe2dbee124085505806828d807f78
Nick Zhang <zhangnick98@gmail.com>
2022-11-25 18:26:59 +0100

    add generation func for dir/undirected graphs

--- src/generation.py
+++ src/generation.py
@@ -1,8 +1,9 @@
 import numpy as np
-import random
 
-def generate_graph(N, S, thetas = 'random', type = 'undirected'):
+def generate_graph(S, thetas = 'random', type = 'undirected', ret_thetas = False):
+    N = np.sum(S)
     K = len(S)
+
     H = []
     c = 0
     for i in S:
@@ -10,50 +11,33 @@ def generate_graph(N, S, thetas = 'random', type = 'undirected'):
             H += [c]
         c+=1
 
+    if np.all(thetas) == 'random':
+        thetas = np.zeros((K,K))
+        for i in range(K):
+            for j in range(K):
+                thetas[i, j] = np.random.uniform()
+        
     if type == 'undirected':
-        if thetas == 'random':
-            thetas = np.zeros((K,K))
-            for i in range(K):
-                for j in range(K):
-                    thetas[i, j] = np.random.uniform()
-            trThetas = np.triu(thetas)
-            thetas = np.where(trThetas, trThetas, trThetas.T)
+        trThetas = np.triu(thetas)
+        thetas = np.where(trThetas, trThetas, trThetas.T)
 
-        for i in range(N):
-            for j in range(N):
-                if i == j:
-                    continue
-                X[i, j] = np.random.binomial(1, thetas[H[i], H[j]]) #Draw edge from theta cluster probabilities
+    #TODO: assert theta valid
 
+    X = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            if i == j:
+                continue
+            X[i, j] = np.random.binomial(1, thetas[H[i], H[j]]) #Draw edge from theta cluster probabilities
+
+    if type == 'undirected':
         trX = np.triu(X)
         X = np.where(trX, trX, trX.T) #make X symmetric
-    return X
-
-N = 10
-X = np.zeros((N,N))
-K = 2
-S = [4, 6] #Z collapsed, ordered
-
-H = []
-c = 0
-for i in S:
-    for _ in range(i):
-        H += [c]
-    c+=1
     
-thetas = np.array([[0.5, 0.8],
-                    [0.8, 0.3]])
-
-
-for i in range(N):
-    for j in range(N):
-        if i == j:
-            continue
-        X[i, j] = np.random.binomial(1, thetas[H[i], H[j]])
-
-trX = np.triu(X)
-X = np.where(trX, trX, trX.T)
-
+    if ret_thetas:
+        return X, thetas
+    else:
+        return X
 
 
 

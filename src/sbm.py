3a4c838e82188ac5f4d78f2779203bef6fe8124f
Nick Zhang <zhangnick98@gmail.com>
2023-02-04 19:46:54 +0100

    add directed weighted

--- src/sbm.py
+++ src/sbm.py
@@ -30,6 +30,10 @@ class smb():
 
     def fit(self, X, T):
         self.X = X
+
+        self.X_bin = self.X.copy()
+        self.X_bin[X_bin>0] = 1
+
         self.T = T
         self.N = len(self.X)
 
@@ -46,7 +50,39 @@ class smb():
         self.idx_list = [x for x in range(self.N)]
         for _ in range(self.T):
             for n in range(self.N):
-                self.gibbs_sweep(n, directed, binary)
+                nn = list(self.idx_list)
+                nn.remove(n) #nn = index mask without currently sampled node n
+
+                X_ = self.X[np.ix_(nn,nn)] #adjacency matrix without currently sampled node
+
+                K = len(self.z[0])  # K = n. of components
+
+                # Delete empty component if present
+                if K > 1:
+                    idx = np.argwhere(np.sum(self.z[nn], 0) == 0)
+                    self.z = np.delete(self.z, idx, axis=1)
+                    K -= len(idx)
+
+                loglikelihood = self.evalLikelihood(nn, K, self.directed, self.binary):
+
+                logPrior = np.log(np.append(m, self.A)) #TODO: Priors
+
+                logPosterior = logPrior + logLikelihood
+
+                # Convert from log probabilities, normalized to max
+                p = np.exp(logPosterior-max(logPosterior)) 
+
+                # Assignment through random draw fron unif(0,1), taking first value from prob. vector
+                draw = np.random.rand()
+                i = np.argwhere(draw<np.cumsum(p)/sum(p))[0]
+
+                # Assignment of current node to component i
+                self.z[n,:] = 0
+                if i == K: # If new component: add new column to partition matrix
+                    self.z = np.hstack([self.z, np.zeros(self.N,1)]) 
+                self.z[n,i] = 1
+
+                # self.gibbs_sweep(n, directed, binary)
 
             self.Z.append(self.z.copy())
 
@@ -58,6 +94,7 @@ class smb():
         self.Zc = []
 
         self.idx_list = [x for x in range(self.N)]
+        
         for _ in range(self.T):
             for n in range(self.N):
                 self.gibbs_sweep_bicluster(n, "rows", directed, binary)
@@ -169,21 +206,9 @@ class smb():
             if i == Kc: # If new component: add new column to partition matrix
                 self.zc = np.hstack([self.zc, np.zeros(self.N,1)]) 
             self.zc[n,i] = 1
-        
-
-    def gibbs_sweep(self, n, directed, binary):
-        nn = list(self.idx_list)
-        nn.remove(n) #nn = index mask without currently sampled node n
-
+    
+    def evalLikelihood(nn, K, directed, binary):
         X_ = self.X[np.ix_(nn,nn)] #adjacency matrix without currently sampled node
-
-        K = len(self.z[0])  # K = n. of components
-
-        # Delete empty component if present
-        if K > 1:
-            idx = np.argwhere(np.sum(self.z[nn], 0) == 0)
-            self.z = np.delete(self.z, idx, axis=1)
-            K -= len(idx)
 
         if directed:
             if binary: #directed binary
@@ -230,8 +255,80 @@ class smb():
                 logLikelihood = np.concatenate([logLikelihood_old, logLikelihood_new])
                 
             else: #directed weighted
+                m = np.sum(self.z[nn,:], 0)[np.newaxis]
 
-                pass
+                r = self.z[nn,:].T @ self.X[n, nn]
+                R = np.tile(r, (K, 1))
+
+                s = self.z[nn,:].T @ self.X[nn, n]
+                S = np.tile(s[np.newaxis].T, (1, K))
+
+                M = np.tile(m, (K, 1)) + np.diag(m.flatten())
+
+                M1 = self.z[nn,:].T @ X_ @ self.z[nn,:]
+                M2 = M1.T[~np.eye(M1.T.shape[0],dtype=bool)].reshape(M1.T.shape[0], -1).copy()
+
+                LM = np.concatenate([M1,M2],axis=1) #Link Matrix
+                LM_n = np.zeros((LM.shape[0], LM.shape[1])) #Link Matrix of current node n
+                LM_n[0:R.shape[0], 0:R.shape[1]] += R
+                s_diag = np.diag(s.flatten())
+                LM_n[0:s_diag.shape[0], 0:s_diag.shape[1]] += s_diag
+                S = S.T[~np.eye(S.shape[0],dtype=bool)].reshape(S.shape[0], -1)
+                if K > 1: 
+                    LM_n[:,-S.shape[1]:] += S
+
+                M__2 = M[~np.eye(M.shape[0],dtype=bool)].reshape(M.shape[0], -1)
+                max_links_current_node = np.concatenate([M,M__2],axis=1)
+
+                
+                # Section for C
+                X_bin_ = self.X_bin[np.ix_(nn,nn)]
+
+                r_bin = self.z[nn,:].T @ self.X_bin[n, nn]
+                R_bin = np.tile(r_bin, (K, 1))
+
+                s_bin = self.z[nn,:].T @ self.X_bin[nn, n]
+                S_bin = np.tile(s_bin[np.newaxis].T, (1, K))
+
+                C1 = self.z[nn,:].T @ X_bin_ @ self.z[nn,:]
+                C2 = C1.T[~np.eye(C1.T.shape[0],dtype=bool)].reshape(C1.T.shape[0], -1).copy()
+                C = np.concatenate([C1, C2], axis=1) #no. of possible links
+
+                LM_n_bin = np.zeros((LM.shape[0], LM.shape[1]))
+                LM_n_bin[0:R_bin.shape[0], 0:R_bin.shape[1]] += R_bin
+                s_diag_bin = np.diag(s_bin.flatten())
+                LM_n_bin[0:s_diag_bin.shape[0], 0:s_diag_bin.shape[1]] += s_diag_bin
+                S_bin = S_bin.T[~np.eye(S_bin.shape[0],dtype=bool)].reshape(S_bin.shape[0], -1)
+                if K > 1: 
+                    LM_n_bin[:,-S_bin.shape[1]:] += S_bin
+                #End section for C
+
+                F = np.zeros((LM.shape[0], LM.shape[1]))
+                f_out = np.sum(gammaln(np.multiply(self.z[nn,:].T, self.X[n, nn]) + 1), axis = 1)
+                F_out = np.tile(f_out, (K, 1))
+
+                f_in = np.sum(gammaln(np.multiply(self.z[nn,:].T, self.X[nn, n]) + 1), axis = 1)
+                f_in_diag = np.diag(f_in)
+                F_in = np.tile(f_in[np.newaxis].T, (1, K))
+                F_in = F_in.T[~np.eye(F_in.shape[0],dtype=bool)].reshape(F_in.shape[0], -1)
+
+                F[0:F_out.shape[0], 0:F_out.shape[1]] += F_out
+                F[0:F_out.shape[0], 0:F_out.shape[1]] += f_in_diag
+                if K > 1: 
+                    F[:,-F_in.shape[1]:] += F_in
+                if K == 1:
+                    F[:] += f_in
+
+
+                logLikelihood_old = np.sum(gammaln(LM + LM_n + self.a) - gammaln(LM + self.a) \
+                            + (LM + self.a)*np.log(C + self.b) - (LM + LM_n + self.a)*np.log(C + LM_n_bin + self.b) \
+                            - F, 1)
+
+                logLikelihood_new = np.sum(gammaln(np.hstack([r,s]) + self.a) - gammaln(self.a) \
+                            + (self.b)*np.log(self.a) - (np.hstack([r,s]) + self.a)*np.log(np.hstack([r_bin,s_bin]) + self.b) \
+                            - np.hstack([f_out, f_in]))[np.newaxis]
+
+                logLikelihood = np.concatenate([likelihood, likelihood_n])                
         else:
             if binary: #undirected binary
                 # m = n. of nodes in each component 
@@ -258,22 +355,111 @@ class smb():
             else: #undirected weighted
                 pass
 
+        return logLikelihood
+
+    # def gibbs_sweep(self, n, directed, binary):
+    #     nn = list(self.idx_list)
+    #     nn.remove(n) #nn = index mask without currently sampled node n
+
+    #     X_ = self.X[np.ix_(nn,nn)] #adjacency matrix without currently sampled node
+
+    #     K = len(self.z[0])  # K = n. of components
+
+    #     # Delete empty component if present
+    #     if K > 1:
+    #         idx = np.argwhere(np.sum(self.z[nn], 0) == 0)
+    #         self.z = np.delete(self.z, idx, axis=1)
+    #         K -= len(idx)
+
+    #     if directed:
+    #         if binary: #directed binary
+    #             # m = n. of nodes in each component 
+    #             m = np.sum(self.z[nn,:], 0)[np.newaxis] #newaxis allows m to become 2d array (for transposing)
+                
+    #             M1 = self.z[nn,:].T @ X_ @ self.z[nn,:] #n. of links between components without current node
+
+    #             # r = n. of links from current node to components
+    #             r = self.z[nn,:].T @ self.X[n, nn]
+    #             R = np.tile(r, (K, 1))
+
+    #             # s = n. of links from components to current node
+    #             s = self.z[nn,:].T @ self.X[nn, n]
+    #             S = np.tile(s[np.newaxis].T, (1, K))
+
+    #             M2 = M1.T[~np.eye(M1.T.shape[0],dtype=bool)].reshape(M1.T.shape[0], -1).copy()
+    #             LM = np.concatenate([M1,M2],axis=1) #Link Matrix
+
+    #             LM_n = np.zeros((LM.shape[0], LM.shape[1])) #LM of current node n
+    #             LM_n[0:R.shape[0], 0:R.shape[1]] += R
+    #             s_diag = np.diag(s.flatten())
+    #             LM_n[0:s_diag.shape[0], 0:s_diag.shape[1]] += s_diag
+    #             S = S.T[~np.eye(S.shape[0],dtype=bool)].reshape(S.shape[0], -1)
+    #             if K > 1: 
+    #                 LM_n[:,-S.shape[1]:] += S
+
+    #             M0 = m.T@m - np.diag(m.flatten()) - M1 #n. of non-links between components without current node
+    #             M0_2 = M0.T[~np.eye(M0.T.shape[0],dtype=bool)].reshape(M0.T.shape[0], -1)
+    #             NLM = np.concatenate([M0, M0_2], axis=1) #Non-Link Matrix
+
+    #             PLM = np.tile(m, (K, 1)) + np.diag(m.flatten()) #Potential links matrix from other clusts
+    #             PLM_2 = PLM[~np.eye(PLM.shape[0],dtype=bool)].reshape(PLM.shape[0], -1)
+    #             P_n = np.concatenate([PLM,PLM_2],axis=1) #Potential links for current node n
+
+    #             logLikelihood_old = np.sum(betaln(LM + LM_n + self.a, NLM + P_n - LM_n + self.b) \
+    #                                  - betaln(LM + self.a, NLM + self.b)
+    #                                    , 1) #log prob of assigning to existing clusters
+
+    #             logLikelihood_new = np.sum(betaln(np.hstack([r,s]) + self.a, np.hstack([m-r,m-s]) + self.b) \
+    #                                      - betaln(self.a, self.b)
+    #                                        , 1) #log prob of assigning to new clusters
+
+    #             logLikelihood = np.concatenate([logLikelihood_old, logLikelihood_new])
+                
+    #         else: #directed weighted
+
+    #             pass
+    #     else:
+    #         if binary: #undirected binary
+    #             # m = n. of nodes in each component 
+    #             m = np.sum(self.z[nn], 0)[np.newaxis]
+    #             P = np.tile(m, (K, 1)) #Potential links
+
+    #             M1 = self.z[nn].T @ X_ @ self.z[nn] - np.diag(np.sum(X_@self.z[nn]*self.z[nn], 0) / 2) #n. of links between components without current node
+
+    #             M0 = m.T@m - np.diag((m*(m+1) / 2).flatten()) - M1 #n. of non-links between components without current node
+
+    #             r = self.z[nn].T @ self.X[nn, n] #n. of links from current node to components
+    #             R = np.tile(r, (K, 1))
+
+    #             logLikelihood_old = np.sum(betaln(M1 + R + self.a, M0 + P - R + self.b) \
+    #                                      - betaln(M1 + self.a, M0 + self.b)
+    #                                        , 1)
+
+    #             logLikelihood_new = np.sum(betaln(r + self.a, m - r + self.b) \
+    #                                      - betaln(self.a, self.b)
+    #                                        , 1)
+
+    #             logLikelihood = np.concatenate([logLikelihood_old, logLikelihood_new])
+
+    #         else: #undirected weighted
+    #             pass
+
         
-        logPrior = np.log(np.append(m, self.A)) #TODO: Priors
+    #     logPrior = np.log(np.append(m, self.A)) #TODO: Priors
 
-        logPosterior = logPrior + logLikelihood
+    #     logPosterior = logPrior + logLikelihood
 
-        # Convert from log probabilities, normalized to max
-        p = np.exp(logPosterior-max(logPosterior)) 
+    #     # Convert from log probabilities, normalized to max
+    #     p = np.exp(logPosterior-max(logPosterior)) 
 
-        # Assignment through random draw fron unif(0,1), taking first value from prob. vector
-        draw = np.random.rand()
-        i = np.argwhere(draw<np.cumsum(p)/sum(p))[0]
+    #     # Assignment through random draw fron unif(0,1), taking first value from prob. vector
+    #     draw = np.random.rand()
+    #     i = np.argwhere(draw<np.cumsum(p)/sum(p))[0]
 
-        # Assignment of current node to component i
-        self.z[n,:] = 0
-        if i == K: # If new component: add new column to partition matrix
-            self.z = np.hstack([self.z, np.zeros(self.N,1)]) 
-        self.z[n,i] = 1
+    #     # Assignment of current node to component i
+    #     self.z[n,:] = 0
+    #     if i == K: # If new component: add new column to partition matrix
+    #         self.z = np.hstack([self.z, np.zeros(self.N,1)]) 
+    #     self.z[n,i] = 1
 
 
--- src/test.py
+++ src/test.py
@@ -303,10 +303,168 @@ g = ig.Graph.Read_Pajek('datasets/Hi-tech.net')
 
 g = ig.Graph.Read_Pajek('datasets/centrality_literature.net')
 
+import igraph as ig
+import numpy as np
+from scipy.special import betaln
+
+def irm_undirected(X, T, a, b, A, set_seed = True, random_seed = 42, print_iter = False):
+    N = len(X)
+    z = np.ones([N,1])
+    Z = []
+
+    if set_seed:
+        np.random.seed(random_seed)
+
+    for t in range(T): # for T iterations
+        if print_iter:
+            print(t)
+        for n in range(N): # for each node n
+
+            #nn = index mask without currently sampled node n
+            nn = [_ for _ in range(N)]  
+            nn.remove(n) 
+
+            X_ = X[np.ix_(nn,nn)] #adjacency matrix without currently sampled node
+
+            # K = n. of components
+            K = len(z[0]) 
+
+            # Delete empty component if present
+            if K > 1:
+                idx = np.argwhere(np.sum(z[nn], 0) == 0)
+                z = np.delete(z, idx, axis=1)
+                K -= len(idx)
+
+            # m = n. of nodes in each component 
+            m = np.sum(z[nn], 0)[np.newaxis]
+            M = np.tile(m, (K, 1))
+            
+
+            # M1 = n. of links between components without current node
+            M1 = z[nn].T @ X_ @ z[nn] - np.diag(np.sum(X_@z[nn]*z[nn], 0) / 2) 
+            
+            # M0 = n. of non-links between components without current node
+            M0 = m.T@m - np.diag((m*(m+1) / 2).flatten()) - M1 
+
+            # r = n. of links from current node to components
+            r = z[nn].T @ X[nn, n]
+            R = np.tile(r, (K, 1))
+
+            # lik matrix of current node sampled to each component
+            likelihood = np.sum(betaln(M1+R+a, M0+M-R+b) - betaln(M1+a, M0+b), 1)
+            # lik of current node to new component
+            likelihood_n = np.sum(betaln(r+a, m-r+b) - betaln(a,b), 1)
+
+            logLik = np.concatenate([likelihood, likelihood_n])
+            logPrior = np.log(np.append(m, A))
+
+            logPost = logPrior + logLik
+
+            # Convert from log probabilities, normalized to max
+            P = np.exp(logPost-max(logPost)) 
+
+            # Assignment through random draw fron unif(0,1), taking first value from prob. vector
+            draw = np.random.rand()
+            i = np.argwhere(draw<np.cumsum(P)/sum(P))[0]
+
+            # Assignment of current node to component i
+            z[n,:] = 0
+            if i == K: # If new component: add new column to partition matrix
+                z = np.hstack((z, np.zeros((N,1)))) 
+            z[n,i] = 1
+
+        # Delete empty component if present
+        idx = np.argwhere(np.all(z[..., :] == 0, axis=0))
+        z = np.delete(z, idx, axis=1)    
+
+        Z.append(z.copy())
+
+    return Z 
 
 
+g = ig.Graph.Read_GML('karate.txt')
 
+X = np.array(g.get_adjacency().data)
 
+T = 500
+a = 1
+b = 1
+A = 1
 
+Z = irm_undirected(X, T, a, b, A)
 
+def irm_undirected_old(X, T, a, b, A, set_seed = True, random_seed = 42, print_iter = False):
+    N = len(X)
+    z = np.ones([N,1])
+    Z = []
 
+    if set_seed:
+        np.random.seed(random_seed)
+
+    for t in range(T): # for T iterations
+        if print_iter:
+            print(t)
+        for n in range(N): # for each node n
+
+            #nn = index mask without currently sampled node n
+            nn = [_ for _ in range(N)]  
+            nn.remove(n) 
+
+            X_ = X[np.ix_(nn,nn)] #adjacency matrix without currently sampled node
+
+            # K = n. of components
+            K = len(z[0]) 
+
+            # Delete empty component if present
+            if K > 1:
+                idx = np.argwhere(np.sum(z[nn], 0) == 0)
+                z = np.delete(z, idx, axis=1)
+                K -= len(idx)
+
+            # m = n. of nodes in each component 
+            m = np.sum(z[nn], 0)[np.newaxis]
+            M = np.tile(m, (K, 1))
+            
+
+            # M1 = n. of links between components without current node
+            M1 = z[nn].T @ X_ @ z[nn] - np.diag(np.sum(X_@z[nn]*z[nn], 0) / 2) 
+            
+            # M0 = n. of non-links between components without current node
+            M0 = m.T@m - np.diag((m*(m+1) / 2).flatten()) - M1 
+
+            # r = n. of links from current node to components
+            r = z[nn].T @ X[nn, n]
+            R = np.tile(r, (K, 1))
+
+            # lik matrix of current node sampled to each component
+            likelihood = betaln(M1+R+a, M0+M-R+b) - betaln(M1+a, M0+b)
+            # lik of current node to new component
+            likelihood_n = betaln(r+a, m-r+b) - betaln(a,b)
+
+            logLik = np.sum(np.concatenate([likelihood, likelihood_n]), 1)
+            logPrior = np.log(np.append(m, A))
+
+            logPost = logPrior + logLik
+
+            # Convert from log probabilities, normalized to max
+            P = np.exp(logPost-max(logPost)) 
+
+            # Assignment through random draw fron unif(0,1), taking first value from prob. vector
+            draw = np.random.rand()
+            i = np.argwhere(draw<np.cumsum(P)/sum(P))[0]
+
+            # Assignment of current node to component i
+            z[n,:] = 0
+            if i == K: # If new component: add new column to partition matrix
+                z = np.hstack((z, np.zeros((N,1)))) 
+            z[n,i] = 1
+
+        # Delete empty component if present
+        idx = np.argwhere(np.all(z[..., :] == 0, axis=0))
+        z = np.delete(z, idx, axis=1)    
+
+        Z.append(z.copy())
+
+    return Z 
+
+Z2 = irm_undirected_old(X,T,a,b,A)
